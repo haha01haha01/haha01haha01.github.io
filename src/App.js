0a0b0c042e4d44ed24c9558321b43d3c592db0fb
haha01haha01 <haha01haha01@gmail.com>
2019-08-02 14:46:19 +0300

    First React prototype

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import HaLogo from './HaLogo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={HaLogo} className="App-logo" alt="logo" />
+        <p>
+          I am leet robot
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
